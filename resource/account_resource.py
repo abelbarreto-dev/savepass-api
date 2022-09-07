cc471e265bfde2562763111036c49cb3f70fe7b9
Abel Carvalho <64908368+carvalhoabel@users.noreply.github.com>
2022-09-07 18:08:17 -0300

    refactor: routes await call and account get login added (#18)
    * feat: get login method, refactor: await methods
    
    * refactor: await methods route login note
    
    * refactor: await methods route login
    
    * refactor: await methods route note
    
    * refactor: await methods route password

--- resource/account_resource.py
+++ resource/account_resource.py
@@ -7,7 +7,10 @@ class AccountResource:
     async def create_account(self, account: Account) -> json:
         pass
 
-    async def update_account(self, account: Account) -> json:
+    async def get_account_login(self, username: str, password: str) -> json:
+        pass
+
+    async def update_account(self, username: str, password: str, mobile: str) -> json:
         pass
 
     async def delete_account(self, id: int = 0) -> json:
--- routes/route_note.py
+++ routes/route_note.py
@@ -12,29 +12,29 @@ note_resource = NoteResource()
 
 @route_note.post(note_routes['new'])
 async def post_note(note: Note) -> json:
-    return note_resource.create_note(note=note)
+    return await note_resource.create_note(note=note)
 
 
 @route_note.get(note_routes['get_id'])
 async def get_by_id_note(id: int = 0) -> json:
-    return note_resource.get_note_by_id(id=id)
+    return await note_resource.get_note_by_id(id=id)
 
 
 @route_note.get(note_routes['get_all'])
 async def get_all_notes(id: int = 0) -> json:
-    return note_resource.get_notes_all(id=id)
+    return await note_resource.get_notes_all(id=id)
 
 
 @route_note.get(note_routes['search'])
 async def get_search_notes(**kwargs) -> json:
-    return note_resource.get_notes_search(**kwargs)
+    return await note_resource.get_notes_search(**kwargs)
 
 
 @route_note.put(note_routes['update'])
 async def put_note(note: Note) -> json:
-    return note_resource.update_note(note=note)
+    return await note_resource.update_note(note=note)
 
 
 @route_note.delete(note_routes['delete'])
 async def del_note(id: int = 0) -> json:
-    return note_resource.delete_note(id=id)
+    return await note_resource.delete_note(id=id)
