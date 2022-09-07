6600deea3bbd2216cabe6b24f45b730286a8d021
carvalhoabel <abelbdecarvalho@gmail.com>
2022-09-07 18:04:01 -0300

    refactor: await methods route login

--- routes/route_login.py
+++ routes/route_login.py
@@ -12,29 +12,29 @@ log_resource = LoginResource()
 
 @route_login.post(log_routes['new'])
 async def post_login(login: Login) -> json:
-    return log_resource.create_login(login=login)
+    return await log_resource.create_login(login=login)
 
 
 @route_login.get(log_routes['get_id'])
 async def get_by_id_login(id: int = 0) -> json:
-    return log_resource.get_login_by_id(id=id)
+    return await log_resource.get_login_by_id(id=id)
 
 
 @route_login.get(log_routes['get_all'])
 async def get_all_logins(id: int = 0) -> json:
-    return log_resource.get_logins_all(id=id)
+    return await log_resource.get_logins_all(id=id)
 
 
 @route_login.get(log_routes['search'])
 async def get_search_login(**kwargs) -> json:
-    return log_resource.get_login_search(**kwargs)
+    return await log_resource.get_login_search(**kwargs)
 
 
 @route_login.put(log_routes['update'])
 async def put_login(login: Login) -> json:
-    return log_resource.update_login(login=login)
+    return await log_resource.update_login(login=login)
 
 
 @route_login.delete(log_routes['delete'])
 async def del_login(id: int = 0) -> json:
-    return log_resource.delete_login(id=id)
+    return await log_resource.delete_login(id=id)
