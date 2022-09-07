6dd38ee8611649eaba810e9e6281e24b23431c6d
Abel Carvalho <64908368+carvalhoabel@users.noreply.github.com>
2022-09-07 15:36:50 -0300

    feat: exceptions to raise created (#16)

---
+++ controller/exceptions/exceptions.py
@@ -0,0 +1,46 @@
+class NoneFound(Exception):
+    pass
+
+
+class MobileDataType(ValueError):
+    pass
+
+
+class StrExpectedType(TypeError):
+    pass
+
+
+class UsernameError(ValueError):
+    pass
+
+
+class EmailError(ValueError):
+    pass
+
+
+class PasswordError(ValueError):
+    pass
+
+
+class MobileError(ValueError):
+    pass
+
+
+class AccountNotFound(ValueError):
+    pass
+
+
+class NoteNotFound(ValueError):
+    pass
+
+
+class LoginNotFound(ValueError):
+    pass
+
+
+class PasswordNotFound(ValueError):
+    pass
+
+
+class LoginNoteNotFound(ValueError):
+    pass
