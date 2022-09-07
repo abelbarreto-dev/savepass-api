d9d6de12c4e5e5525bef3cbe18aca73823406db0
carvalhoabel <abelbdecarvalho@gmail.com>
2022-09-07 18:03:07 -0300

    refactor: await methods route login note

--- routes/route_loginnote.py
+++ routes/route_loginnote.py
@@ -12,29 +12,29 @@ login_note_resource = LoginNoteResource()
 
 @route_loginnote.post(loginnote_routes['new'])
 async def post_login_note(loginote: LoginNote) -> json:
-    return login_note_resource.create_login_note(loginote=loginote)
+    return await login_note_resource.create_login_note(loginote=loginote)
 
 
 @route_loginnote.get(loginnote_routes['get_id'])
 async def get_by_id_login_note(id: int = 0) -> json:
-    return login_note_resource.get_login_note_by_id(id=id)
+    return await login_note_resource.get_login_note_by_id(id=id)
 
 
 @route_loginnote.get(loginnote_routes['get_all'])
 async def get_all_login_notes(id: int = 0) -> json:
-    return login_note_resource.get_login_notes_all(id=id)
+    return await login_note_resource.get_login_notes_all(id=id)
 
 
 @route_loginnote.get(loginnote_routes['search'])
 async def get_search_login_note(**kwargs) -> json:
-    return login_note_resource.get_login_note_search(**kwargs)
+    return await login_note_resource.get_login_note_search(**kwargs)
 
 
 @route_loginnote.put(loginnote_routes['update'])
 async def put_login_note(loginnote: LoginNote) -> json:
-    return login_note_resource.udpate_login_note(loginnote=loginnote)
+    return await login_note_resource.udpate_login_note(loginnote=loginnote)
 
 
 @route_loginnote.delete(loginnote_routes['delete'])
 async def del_login_note(id: int = 0) -> json:
-    return login_note_resource.delete_login_note(id=id)
+    return await login_note_resource.delete_login_note(id=id)
