6e72a5f123b1397055dd08060740f51c8beccc62
carvalhoabel <abelbdecarvalho@gmail.com>
2022-09-07 21:36:00 -0300

    feat: checkers and encrypt and decrypt attributes

--- resource/password_resource.py
+++ resource/password_resource.py
@@ -1,8 +1,26 @@
 import json
 from models.password import Password
+from crypter.encrypter import Encrypt
+from crypter.decrypter import Decrypt
+from controller.exceptions.exceptions import IDError, PasswordError
+from controller.tools.checker import password_checker
 
 
 class PasswordResource:
+
+    PASSWORD: str = 'password'
+
+    def __init__(self):
+        self._encrypt = Encrypt()
+        self._decrypt = Decrypt()
+
+    @property
+    def encrypt(self) -> Encrypt:
+        return self._encrypt
+
+    @property
+    def decrypt(self) -> Decrypt:
+        return self._decrypt
 
     async def create_password(self, password: Password) -> json:
         pass
@@ -21,3 +39,17 @@ class PasswordResource:
 
     async def delete_password(self, id: int = 0) -> json:
         pass
+
+    # utils methods
+
+    def _checker_data_password(self, password: str) -> None:
+        if not isinstance(password, str):
+            raise PasswordError('password type must be string.')
+        if not password_checker(password=password):
+            raise PasswordError('password is invalid.')
+
+    def _checker_id(self, id: int, name: str = 'id') -> None:
+        if not isinstance(id, int):
+            raise IDError(f'{name} type must be string.')
+        if not id < 1:
+            raise IDError(f'{name} must be positive.')
