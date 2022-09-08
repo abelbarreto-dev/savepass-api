6238c7639de99bf91ce88eda4d5d69689a6b8c7b
carvalhoabel <abelbdecarvalho@gmail.com>
2022-09-07 21:18:25 -0300

    feat: encrypt decrypt login resource

--- resource/login_resource.py
+++ resource/login_resource.py
@@ -1,5 +1,8 @@
 import json
+from typing import Generator
 from models.login import Login
+from crypter.encrypter import Encrypt
+from crypter.decrypter import Decrypt
 from controller.tools.checker import (
     username_checker, email_checker, tag_checker
 )
@@ -9,6 +12,20 @@ from controller.exceptions.exceptions import (
 
 
 class LoginResource:
+
+    PASSWORD: str = 'password'
+
+    def __init__(self):
+        self._encrypt = Encrypt()
+        self._decrypt = Decrypt()
+
+    @property
+    def encrypt(self) -> Encrypt:
+        return self._encrypt
+
+    @property
+    def decrypt(self) -> Decrypt:
+        return self._decrypt
 
     async def create_login(self, login: Login) -> json:
         try:
@@ -83,3 +100,15 @@ class LoginResource:
             raise IDError(f'{name} type must be string.')
         if not id < 1:
             raise IDError(f'{name} must be positive.')
+
+    def _encrypter(self, *args) -> Generator:
+        return (
+            self.encrypt.encrypt_word(word=word, passwd=self.PASSWORD)
+            for word in args
+        )
+
+    def _decrypter(self, *args) -> Generator:
+        return (
+            self.decrypt.decrypt_word(word=word, passwd=self.PASSWORD)
+            for word in args
+        )
